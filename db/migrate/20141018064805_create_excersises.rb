87740c8b971bc029d291d03bd186302c23d94ca6
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-10-18 20:02:39 +1300

    Excersise Model DB Migration Wrote the migration file for the Excersise model

--- db/migrate/20141018064805_create_excersises.rb
+++ db/migrate/20141018064805_create_excersises.rb
@@ -1,6 +1,11 @@
 class CreateExcersises < ActiveRecord::Migration
   def change
     create_table :excersises do |t|
+		t.string "name", limit => "100"
+		t.integer "type"
+		t.float "diffculty"
+		t.integer "group", :defualt => 0
+		t.integer "position"
 
       t.timestamps
     end

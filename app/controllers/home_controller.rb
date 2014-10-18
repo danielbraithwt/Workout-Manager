0ca7b30e9b475bb82f5c5999dde345abfd7e4067
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-10-18 21:23:58 +1300

    Generated Workouts Controller I generated the files for the workouts controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
 	  puts "Home Controller : INDEX"
+
+	  @workouts = Workout.all
+
+	  puts "There are #{@workouts.size} workouts"
   end
 end

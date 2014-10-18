1b4c9ab772b612f9c1496328c1bd9d2378a92cf6
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-10-18 22:21:58 +1300

    Create Workouts Workouts can now be created however all that can be given to them is there name

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,4 +1,34 @@
 class WorkoutsController < ApplicationController
-  def new
-  end
+
+	def show
+		@workout = Workout.find(params[:id])
+		@excersises = @workout.excersises
+
+	end
+
+  	def new
+		
+  	end
+
+ 	def create
+		puts "Creating workout with '#{params[:workout]}'"
+
+		# TODO: Make so can do mass assignment
+		w = Workout.new(:name => params[:workout][:name])
+		w.save
+
+		if w.save
+			redirect_to :action => "edit", :id => w.id
+		else
+			puts "Error while saving workout"
+			redirect_to :controller => "home", :action => "index"
+		end
+  	end
+
+	def edit
+		puts "Editing Workout : #{params[:id]}"
+
+		@workout = Workout.find(params[:id])
+
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'workouts/new'
+  #get 'workouts/new'
 
-  root 'home#index'
+ 	root 'home#index'
+
+	match ':controller(/:action(/:id))', :via => [:get, :post]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

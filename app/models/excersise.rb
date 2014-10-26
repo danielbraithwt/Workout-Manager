32fdeb241c2016ee25dbf2cf297fdc98c075a2be
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-10-26 16:22:10 +1300

    Added Units And Changed Auto Saving I added in diffrent units for the diffculty depending on what excersise type was chosen. Also now the auto save to the database will happen on the key up event

--- app/models/excersise.rb
+++ app/models/excersise.rb
@@ -2,4 +2,14 @@ class Excersise < ActiveRecord::Base
 
 	belongs_to :workout
 
+	def get_units
+		if excersisetype == 1
+			return "kg"
+		elsif excersisetype == 2
+			return "sec"
+		else
+			return ""
+		end
+	end
+
 end

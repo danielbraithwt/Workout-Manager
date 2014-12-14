2c85019b0d5d04fb97d397011a8decf33ab4a307
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-12-14 16:27:38 +1300

    Index Page Now if you are loged in and try visit the home page you will just be redirected to the loged in index page

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,13 @@ class IndexController < ApplicationController
 	layout false
 
 	def index
-		
+
+		# If the user is allready logged in then redirect the to the loged
+		# in home page
+		if session[:user_id] != nil
+			redirect_to(:controller => 'home', :action => 'index')
+		end
+	
 	end
 
 end

2a7f358d0596c8d4fcd022ae146c9fad1f0ed678
Daniel Braithwaite <danielbraithwt@gmail.com>
2014-12-29 11:28:06 +1300

    Changed How Mobile Session Was Identified Origonally the user would only be taken to the mobile site if the requested to be, now they will be if there browser is identified as a mobile one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,28 @@ class ApplicationController < ActionController::Base
 
 	private
 
+	MOBILE_BROWSERS = ["android", "ipod", "opera mini", "blackberry", "palm","hiptop","avantgo","plucker", "xiino","blazer","elaine", "windows ce; ppc;", "windows ce; smartphone;","windows ce; iemobile", "up.browser","up.link","mmp","symbian","smartphone", "midp","wap","vodafone","o2","pocket","kindle", "mobile","pda","psp","treo"]
+
 	def mobile_device?
 		if session[:mobile_param]
+			session[:mobile_param] = 1
+		elsif mobile_browser?
 			session[:mobile_param] = 1
 		else
 			request.user_ages =~ /Mobile|webOS/
 		end
 	end
 	helper_method :mobile_device?
+
+	def mobile_browser?
+		agent = request.headers["HTTP_USER_AGENT"].downcase
+
+		MOBILE_BROWSERS.each do |browser|
+			return true if agent.match(browser)
+		end
+
+		return false
+	end
 
 	def prepare_for_mobile
 		session[:mobile_param] = params[:mobile] if params[:mobile]
